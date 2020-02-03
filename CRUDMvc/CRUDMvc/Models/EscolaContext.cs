267dd29e4b2de0807ba6dca4431d24803abe991f
João <joaoferraz@hotmail.com>
2020-02-03 16:11:15 -0300

    first commit

---
+++ CRUDMvc/CRUDMvc/Models/EscolaContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+
+namespace CrudAspNetMvc.Models
+{
+    public class EscolaContext : DbContext
+    {
+        public EscolaContext() : base("Escola")
+        {
+            Database.CreateIfNotExists();
+        }
+        public DbSet<Aluno> Alunos { get; set; }
+
+    }
+}
